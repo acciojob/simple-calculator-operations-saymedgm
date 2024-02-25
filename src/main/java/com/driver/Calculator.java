3916be408b415632a880967733f74e91cb64e7cd
saymedgm <deepak.cse.dtu@gmail.com>
2024-02-25 18:20:04 +0530

    1

--- src/main/java/com/driver/Calculator.java
+++ src/main/java/com/driver/Calculator.java
@@ -1,23 +1,22 @@
 package com.driver;
 
 public class Calculator {
-	public int add(int num1, int num2) {
-		// your code goes here
+    public int add(int num1, int num2) {
         return num1 + num2;
     }
 
     public int subtract(int num1, int num2) {
-    	// your code goes here
         return num1 - num2;
     }
 
     public int multiply(int num1, int num2) {
-    	// your code goes here
         return num1 * num2;
     }
 
     public double divide(int num1, int num2) {
-    	// your code goes here
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
         return (double) num1 / num2;
     }
 }
